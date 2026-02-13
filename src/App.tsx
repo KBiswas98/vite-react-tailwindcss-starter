8cf4ff288ae5744921112f571f3141e2633168cc
Kamalesh Biswas <38280417+KBiswas98@users.noreply.github.com>
2026-02-13 04:16:03 +0000

    base

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen w-screen bg-orange-400'>
+        <p className='text-xl font-black'>Hiii</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
